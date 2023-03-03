0064c861443006e4cbeba69e8e5fdd245dde5888
ChanwooPark <chanw9503@gmail.com>
2023-03-03 11:10:38 +0900

    reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,14 @@
-import './App.css';
-import styled from 'styled-components';
-import TestPage from './component/TestPage';
-import GlobalStyle from './GlobalStyle';
-
-const StContainer = styled.div`
-  display: flex;
-`;
-
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor};
-  margin: 20px;
-`;
-
-//박스의 색
-const boxList = ['red', 'blue', 'green', 'b'];
-
-//색을 넣으면, 이름을 반환
-const getBoxName = (color) => {
-  switch (color) {
-    case 'red':
-      return '빨간박스';
-    case 'blue':
-      return '파란박스';
-    case 'green':
-      return '초록박스';
-    default:
-      return '검정박스';
-  }
-};
+import React from 'react';
+import './reset.css';
 
 function App() {
   return (
-    // <StContainer>
-    //   {/* <StBox borderColor="red">빨간박스</StBox> */}
-    //   {boxList.map((box) => {
-    //     return <StBox borderColor={box}>{getBoxName(box)}</StBox>;
-    //   })}
-    // </StContainer>
-    <>
-      <GlobalStyle />
-      <TestPage title="제목입니다." contents="내용입니다." />
-    </>
+    <div>
+      <span>Default Style 테스트</span>
+      <h1>이건 h1 태그입니다.</h1>
+      <p>나는 피태그 입니다.</p>
+    </div>
   );
-
-  //props : 부모 컴 -> 자식 컴
 }
 
 export default App;
